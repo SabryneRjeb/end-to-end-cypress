33eccf2f1aa6d019e55ae47f7f5fff560a794418
SabryneRjeb <sabryne.rjeb@gmail.com>
2022-06-23 17:56:11 -0400

    main commit

---
+++ cypress/e2e/test_case1/test.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Login functionality',() =>{
+
+    it('should get to the main page', () =>{
+      
+        cy.visit('https://www.amazon.com/')
+
+
+    })
+
+} );
